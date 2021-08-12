b88949a7b7c2750bbe1da8867476119a960d4952
Kobus <kobus@infinityarc.net>
2021-08-12 08:56:44 +0200

    new branch and started working on jump counter

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 var character = document.getElementById("character");
-document.addEventListener("click",jump);
-function jump(){
-    if(character.classList == "animate"){return;}
+document.addEventListener("click", jump);
+
+function jump() {
+    if (character.classList == "animate") { return; }
     character.classList.add("animate");
-    setTimeout(removeJump,300) //300ms = length of animation
+    setTimeout(removeJump, 300) //300ms = length of animation
 };
-function removeJump(){
+
+function removeJump() {
     character.classList.remove("animate");
 }
 var block = document.getElementById("block");
-function checkDead(){
+
+function checkDead() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
         alert("You Lose!!");
     }
 }
 setInterval(checkDead, 10);
 
-
-
-
-
+const jumpCount = '';
